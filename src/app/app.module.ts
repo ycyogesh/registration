c42aa39da0ca9e61b58164408a8d1a45d1d484f3
ycyogesh <ycyogesh183@gmail.com>
2022-09-25 14:33:28 +0530

    Started project registration module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LeftsideComponent } from './leftside/leftside.component';
+import { RightsideComponent } from './rightside/rightside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftsideComponent,
+    RightsideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
