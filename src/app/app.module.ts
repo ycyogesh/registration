c84985116840da5608f5ac9ccc419b731e3e1a62
ycyogesh <ycyogesh183@gmail.com>
2022-09-25 20:08:34 +0530

    Implemented signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LeftsideComponent } from './leftside/leftside.component';
-import { RightsideComponent } from './rightside/rightside.component';
 import { SignupComponent } from './signup/signup.component';
-import { LogoComponent } from './logo/logo.component';
 import { MailsentComponent } from './mailsent/mailsent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftsideComponent,
-    RightsideComponent,
     SignupComponent,
-    LogoComponent,
     MailsentComponent
   ],
   imports: [
