cc52eb065d784344f131655fb9907a57c378a4c5
ycyogesh <ycyogesh183@gmail.com>
2022-09-27 08:37:12 +0530

    implemented forgot password page

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotpasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  requestLink(){
+    console.log('Requested');
+    
+  }
+
+  login(){
+    this.router.navigate(['login']);
   }
 
 }
